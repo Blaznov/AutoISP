e72b38a6d92f7421f4b47298b5d065329e33d9e0
moroz <moroz.nastyusha@gmail.com>
2018-12-16 18:37:55 +0200

    without texts

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $(".hide-block").hide()
 
 $(".first-step").click(function(){
-	$(".second-step .hide-block").hide("slow");
-	$(".third-step .hide-block").hide("slow");
-	$(".first-step .hide-block").toggle("slow");
+  $(".second-step .hide-block").hide("slow");
+  $(".third-step .hide-block").hide("slow");
+  $(".first-step .hide-block").toggle("slow");
 });
 
 $(".second-step").click(function(){
-	$(".first-step .hide-block").hide("slow");
-	$(".third-step .hide-block").hide("slow");
-	$(".second-step .hide-block").toggle("slow");
+  $(".first-step .hide-block").hide("slow");
+  $(".third-step .hide-block").hide("slow");
+  $(".second-step .hide-block").toggle("slow");
 });
 
 $(".third-step").click(function(){
-	$(".first-step .hide-block").hide("slow");
-	$(".second-step .hide-block").hide("slow");
-	$(".third-step .hide-block").toggle("slow");
+  $(".first-step .hide-block").hide("slow");
+  $(".second-step .hide-block").hide("slow");
+  $(".third-step .hide-block").toggle("slow");
 });
